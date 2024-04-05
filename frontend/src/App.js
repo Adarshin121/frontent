d5c317c573d4766d83abcb33c7849d33b614d8ac
Adarshin121 <160103951+Adarshin121@users.noreply.github.com>
2024-04-05 12:31:08 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import ViewEmployee from './components/ViewEmployee';
+import AddEmployee from './components/AddEmployee';
+import ViewinTable from './components/ViewinTable';
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <ViewinTable/>
+    <Routes>
+      <Route path='/' element={<ViewEmployee/>}/>
+      <Route path='/add' element={<AddEmployee/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
